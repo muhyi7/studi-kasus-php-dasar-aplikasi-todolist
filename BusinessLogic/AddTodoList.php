bbeb32262f2c7ed18ab720ed41991ebd7b0d976e
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-27 07:33:34 +0700

    Add TodoList

--- BusinessLogic/AddTodoList.php
+++ BusinessLogic/AddTodoList.php
@@ -3,6 +3,11 @@
 /**
  * Menambah todo ke list
  */
-function addTodoList(){
+function addTodoList(string $todo)
+{
+    global $todoList;
 
+    $number = sizeof($todoList) + 1;
+
+    $todoList[$number] = $todo;
 }

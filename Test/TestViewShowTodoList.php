db5ba98ac82bfddce7a6bd16d05447886a71fce3
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-28 19:13:14 +0700

    View Show TodoList-Test

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "../View/ViewShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Eko");
+addTodoList("Kurniawan");
+addTodoList("Khannedy");
+addTodoList("Programmer");
+addTodoList("Zaman");
+addTodoList("Now");
+viewTodoList();
